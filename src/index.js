10279bae340d72b172ccd206b256490a69dc4a9f
GO Lee <golee.dev@gmail.com>
2020-07-11 23:03:58 +0900

    fix css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
-import './App.scss';
+import './App.css';
 
 render(<App />, document.getElementById('root'));
